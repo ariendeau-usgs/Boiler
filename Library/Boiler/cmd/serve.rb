04e2bb668c2a718b02ead5f476384f6df508f824
Austin Riendeau <apriendeau@gmail.com>
2012-04-03 14:58:04 -0600

    changed wget to curl

--- Library/Boiler/cmd/serve.rb
+++ Library/Boiler/cmd/serve.rb
@@ -1,4 +1,4 @@
-module Boiler extend self
+ module Boiler extend self
 	def serve
 		if PROJECT_TYPE == "foundation"
 			url ="http://foundation.zurb.com/files/foundation-download-2.2.zip"
@@ -6,15 +6,15 @@ module Boiler extend self
 			filename ="foundation-download-2.2.zip"
 			FileUtils.mkdir PROJECT_NAME
 			FileUtils.cd PROJECT_NAME
-			system("curl #{url}")
+			system("curl -O #{url}")
 			system("tar -xvf #{filename}")
 			FileUtils.rm_rf filename
 		elsif PROJECT_TYPE == "h5bp"
-			url ="https://github.com/downloads/avintagephoto/html5-boilerplate/h5bp.tar.gz"
+			url ="http://cloud.github.com/downloads/avintagephoto/html5-boilerplate/h5bp.tar.gz"
 			title ="html5-boilerplate"
 			filename ="h5bp.tar.gz"
-			system("curl #{url}")
-			system("tar -xvf #{filename}")
+			system("curl -O #{url}")
+			system("tar -xvzf #{filename}")
 			system("mv h5bp-html5-boilerplate-359a13f #{PROJECT_NAME}")
 			FileUtils.rm_rf filename
 		end
