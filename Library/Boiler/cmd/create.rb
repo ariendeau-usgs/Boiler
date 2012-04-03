02c9b0fcbc9f4b0f1868ca3f116e26935d18e767
Austin Riendeau <apriendeau@gmail.com>
2012-04-03 10:09:21 -0600

    added node project type

--- Library/Boiler/cmd/create.rb
+++ Library/Boiler/cmd/create.rb
@@ -5,18 +5,37 @@ module Boiler extend self
   			puts "Need a type and a name"
   			return 0
   		elsif ARGV.length >= 2
-  			if PROJECT_TYPE == 'php' || PROJECT_TYPE == 'html' 
-  				puts "Creating #{PROJECT_NAME}"
-				FileUtils.mkdir PROJECT_NAME
-				puts "Diving into #{PROJECT_NAME}"
-				FileUtils.cd PROJECT_NAME
-				Boiler.makeDirs
-				Boiler.makeFiles
-				Boiler.fetchLibs
-				puts "==#{PROJECT_NAME}== has been created!"
-	  			puts "Please code responsivly :)"	
-  			end
-  		end
+        case PROJECT_TYPE when 'php' || 'html' 
+          dirs  = [  "css", "js", "inc", "img", "js/libs"]
+          files = ["css/style.css", "js/script.js", ".htaccess", "index.#{PROJECT_TYPE}"]
+          puts "Creating #{PROJECT_NAME}"
+          FileUtils.mkdir PROJECT_NAME
+          puts "Diving into #{PROJECT_NAME}"
+          FileUtils.cd PROJECT_NAME
+          Boiler.makeDirs dirs
+          Boiler.makeFiles files
+          Boiler.fetchLibs("js/libs")
+          puts "==#{PROJECT_NAME}== has been created!"
+          puts "Please code responsivly :)" 
+        when 'node'
+          if ! PROJECT_NAME
+            puts "NAME YOUR PROJECT PLEASE!"
+            return 0
+          end
+          puts "Creating #{PROJECT_NAME}"
+          FileUtils.mkdir PROJECT_NAME
+          puts "Diving into #{PROJECT_NAME}"
+          FileUtils.cd PROJECT_NAME
+          node_dirs  = [ "public", "models", "views", "controllers", "public/css", "public/js", "public/inc", "public/img", "public/js/libs"]
+          Boiler.makeDirs node_dirs
+          node_files = ["server.js", "public/css/style.css", "public/js/script.js", ".htaccess", "views/index.html"]
+          Boiler.makeFiles node_files
+          Boiler.fetchLibs("public/js/libs")
+          puts "==#{PROJECT_NAME}== has been created!"
+          puts "Please code responsivly :)" 
+        end
+                
+       end 
   	end ##END CREATE
 end
 
--- Library/Boiler/global.rb
+++ Library/Boiler/global.rb
@@ -1,14 +1,12 @@
 BOILER_VERSION = 'VERSION :: .01 [dev]'
 module Boiler extend self
-	def makeDirs
-		dirs		=  [ 	"css", "js", "inc", "img", "js/libs"]
+	def makeDirs dirs		
 		dirs.each do |d|
 			puts "Creating the #{d} folder!"
 			FileUtils.mkdir "#{d}"
 		end
 	end
-	def makeFiles
-		webfiles = ["css/style.css", "js/script.js", ".htaccess", "index.#{PROJECT_TYPE}"]
+	def makeFiles webfiles
 		webfiles.each do |f|
 			puts "Creating the #{f} file!"
 			FileUtils.touch "#{f}"
@@ -20,15 +18,23 @@ module Boiler extend self
 	   writeOut.write(open(full_url).read)
 	   writeOut.close
 	end
-	def fetchLibs
+	def fetchLibs custom_path
 		puts "Creating the jQuery library!"
-		Boiler.download("http://code.jquery.com/jquery-1.7.2.min.js", "js/libs/jquery-1.7.2.min.js")
+		Boiler.download("http://code.jquery.com/jquery-1.7.2.min.js", "#{custom_path}/jquery-1.7.2.min.js")
 		puts "Creating the Modernizr library!"
-		Boiler.download("http://www.modernizr.com/downloads/modernizr-2.5.3.js", "js/libs/modernizr-2.5.3.js")
+		Boiler.download("http://www.modernizr.com/downloads/modernizr-2.5.3.js", "#{custom_path}/modernizr-2.5.3.js")
 		puts "Creating the Underscore library!"
-		Boiler.download("http://documentcloud.github.com/underscore/underscore-min.js", "js/libs/underscore-min.js")
+		Boiler.download("http://documentcloud.github.com/underscore/underscore-min.js", "#{custom_path}/underscore-min.js")
 		puts "Creating the Backbone library!"
-		Boiler.download("http://documentcloud.github.com/backbone/backbone-min.js", "js/libs/backbone-min.js")
+		Boiler.download("http://documentcloud.github.com/backbone/backbone-min.js", "#{custom_path}/backbone-min.js")
 	end
-
+	def check_varibles
+		if ! PROJECT_NAME
+         puts "NAME YOUR PROJECT PLEASE!"
+         return 0
+      elsif ! PROJECT_TYPE
+      	puts "Did you forget what type of project that you are making?"
+         return 0
+      end
+	end
 end
